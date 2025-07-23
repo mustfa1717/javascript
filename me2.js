b26dbe572fad6d07c97983a0cd85283ccd9f44a1
mustfa1717 <01fe24bcs234@kletech.ac.in>
2025-07-23 17:17:07 +0000

    conversion

---
+++ me2.js
@@ -0,0 +1,35 @@
+let age ="18ddf"
+let decision =true
+console.log(typeof age);
+console.log(typeof decision);
+
+
+let numb=Number(age)
+console.log(typeof numb);
+console.log(numb)
+
+
+let numbr=true 
+let num=Number(numbr)
+console.log(typeof(num));
+
+// now trying the conversion for the boolean data type
+let Isnumb = 0 
+let booleanIsnumb=Boolean(Isnumb)
+console.log(booleanIsnumb);
+console.log(typeof booleanIsnumb);
+
+//hence as we have used 0 which indicates it as false
+
+// if we use "" then the oytput will be false 
+// if we wright string in that then it will be true
+
+//NOW CONVERSION IN STRINGS
+ let str="12"
+ let strn=String(str)
+ console.log(strn);
+ // ye dekhne me number hai par string me convert ho gaya hai
+ console.log(typeof strn);
+ 
+
+ 
