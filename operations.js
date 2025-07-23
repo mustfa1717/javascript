ef8156606cf584680b95434189796113671d1c57
mustfa1717 <01fe24bcs234@kletech.ac.in>
2025-07-23 17:46:23 +0000

    OPERATIONS

---
+++ operations.js
@@ -0,0 +1,55 @@
+//  OPERATIONS 
+
+let value=3
+let numvalue=-value
+console.log(numvalue);
+ // This is a simple method to change the sign of the given value
+
+ // BASIC OPERATIONS
+
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**2);// double into likhna matlab power 
+ console.log(2/2);
+ console.log(2%2);
+
+
+ //ADDING TWO STRINGS
+
+ let str1="hi"
+ let str2=" Mustafa "
+ let str3 = str1+str2
+ console.log(str3);
+ 
+
+//now a tricky part agar
+// 2 numbers ko add karte hai aur unme se kisi ek ko string jaise likhenge to wo string addition ho jayega
+
+console.log("1"+2);
+console.log(2+"1");
+
+// but if the number are more than two then it will take them as they are written
+console.log(2+3+"5");
+console.log("6"+5+9);
+
+//ANOTHER THING IS :
+console.log((2+3)+9*2);
+
+//increment 
+console.log(true);
+console.log(+true);
+//console.log(true+);// this will not work 
+//like this let us try with false 
+//console.log(-false);//this will also not work 
+
+
+//NOW INCREAMENTING VALUES 
+let age =45
+age++
+console.log(age);
+
+/// THIS WA PRETTY MUCH OF OPERATIONS IN JAVA SCRIPT 
+
+
+
