82e48b2106240dfd230a9b016dd76ffb528056d7
mustfa1717 <01fe24bcs234@kletech.ac.in>
2025-07-23 14:14:49 +0000

    my file2

--- me.js
+++ me.js
@@ -1,1 +1,7 @@
-console.log("mustafa")
+const accountId=12345
+accountEmail="mustafaraza17@gmail.com"
+accountCity="NANDED"
+accountName="MUSTAFA"
+
+console.log(accountId);
+console.table([accountId,accountCity,accountEmail,accountName])
